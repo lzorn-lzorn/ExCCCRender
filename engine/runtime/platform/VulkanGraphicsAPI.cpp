d941e944ce974f9e4817d8a7b65e2a3279f670fd
Lzorn <lzr18535482977@gmail.com>
2024-11-21 22:22:27 +0800

    learn: vulkan

--- engine/runtime/platform/VulkanGraphicsAPI.cpp
+++ engine/runtime/platform/VulkanGraphicsAPI.cpp
@@ -1,5 +1,6 @@
 #include <../../runtime/platform/VulkanGraphicsAPI.hpp>
 #include <memory>
+#include <vulkan/vulkan_enums.hpp>
 #include <vulkan/vulkan_funcs.hpp>
 #include <vulkan/vulkan_structs.hpp>
 
@@ -17,10 +18,15 @@ void VulkanGraphicsAPI::Destroy() {
 
 VulkanGraphicsAPI::VulkanGraphicsAPI() {
     CreateInstance();
+    _pickup_physical_device();
+    _query_queue_family_indices();
+    _create_device();
+    _get_queue();
 }
 
 VulkanGraphicsAPI::~VulkanGraphicsAPI() {
     instance.destroy();
+    device.destroy();
 }
 
 void VulkanGraphicsAPI::_create_instance() {
@@ -39,5 +45,32 @@ void VulkanGraphicsAPI::_create_instance() {
 void VulkanGraphicsAPI::_pickup_physical_device() {
     auto device     = instance.enumeratePhysicalDevices();
     physical_device = device[0];  // ! 这里可以选择 GPU，通过GPU支持的特性
+}
+
+void VulkanGraphicsAPI::_create_device() {
+    vk::DeviceCreateInfo      create_info{};
+    vk::DeviceQueueCreateInfo queue_create_info{};
+    float                     queue_priorities = 1.0f;
+    queue_create_info.setPQueuePriorities(&queue_priorities)
+        .setQueueCount(1)
+        .setQueueFamilyIndex(queues_family_indices.graphics_queue.value());
+    create_info.setQueueCreateInfos(queue_create_info);
+
+    device = physical_device.createDevice(create_info);
+}
+
+void VulkanGraphicsAPI::_query_queue_family_indices() {
+    auto properties = physical_device.getQueueFamilyProperties();
+    for (int i = 0; i < properties.size(); i++) {
+        const auto& property = properties[i];
+        if (property.queueFlags | vk::QueueFlagBits::eGraphics) {
+            queues_family_indices.graphics_queue = i;
+            break;
+        }
+    }
+}
+
+void VulkanGraphicsAPI::_get_queue() {
+    graphics_queue = device.getQueue(queues_family_indices.graphics_queue.value(), 0);
 }
 }  // namespace ExCCCRender::Platform
--- engine/runtime/platform/VulkanGraphicsAPI.hpp
+++ engine/runtime/platform/VulkanGraphicsAPI.hpp
@@ -1,6 +1,9 @@
 #include <../../runtime/platform/GraphicsAPI.hpp>
+#include <cstdint>
 #include <memory>
+#include <optional>
 #include <vulkan/vulkan.hpp>
+#include <vulkan/vulkan_handles.hpp>
 
 namespace ExCCCRender::Platform {
 // 所有图形学API的虚接口
@@ -17,14 +20,24 @@ public:
     }
 
 public:
+    struct QueueFamilyIndices final {
+        std::optional<uint32_t> graphics_queue;
+    };
+
     vk::Instance       instance;
     vk::PhysicalDevice physical_device;
+    vk::Device         device;
+    QueueFamilyIndices queues_family_indices;
+    vk::Queue          graphics_queue;
 
 private:
     VulkanGraphicsAPI();
 
     void _create_instance();
     void _pickup_physical_device();
+    void _create_device();
+    void _query_queue_family_indices();
+    void _get_queue();
 
 private:
     static std::unique_ptr<VulkanGraphicsAPI> instance_;
