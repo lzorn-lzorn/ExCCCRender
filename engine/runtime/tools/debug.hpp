fdadace42f08313cc307b1d57411da3ca335975c
Lzorn <lzr18535482977@gmail.com>
2024-11-25 01:08:45 +0800

    add: debug.hpp

---
+++ engine/runtime/tools/debug.hpp
@@ -0,0 +1,23 @@
+#include "../../runtime/stdheader.h"
+
+namespace ExCCCRender::Tools {  // 定义 debug 宏
+#ifdef NDEBUG                   // release
+    #define LOG(...)
+#else  // debug
+    // LOG 宏
+    #define _LOG(...) log_impl(std::cout, __VA_ARGS__)
+#endif
+
+template <typename... Args>
+void log_impl(std::ostream& os, Args&&... args) {
+    ((os << std::forward<Args>(args) << " "), ...) << '\n';  // 折叠表达式打印每个参数
+}
+
+#if defined(_MSC_VER)
+    #define NOINLINE __declspec(noinline)
+#elif defined(__GNUC__) || defined(__clang__)
+    #define NOINLINE __attribute__((noinline))
+#else
+    #define NOINLINE
+#endif
+}  // namespace ExCCCRender::Tools
