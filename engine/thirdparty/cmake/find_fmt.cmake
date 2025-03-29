30686f751f1d2ac133dfdd81747c610598ef9ec5
lizhuoran <liZhuoran1998@outlook.com>
2025-03-29 17:43:12 +0800

    补充find_fmt.cmake

--- engine/thirdparty/cmake/find_fmt.cmake
+++ engine/thirdparty/cmake/find_fmt.cmake
@@ -1,5 +1,3 @@
-
 add_subdirectory(${ExCCCRender_THIRDPARTY_DIR}/fmt)
-# ${PROJECT_NAME} == thirdparty
 target_link_libraries(${PROJECT_NAME} PUBLIC fmt::fmt)
-target_include_directories(thirdparty PUBLIC ${ExCCCRender_THIRDPARTY_DIR})
+target_include_directories(${PROJECT_NAME} PUBLIC ${ExCCCRender_THIRDPARTY_DIR}/fmt/include)
