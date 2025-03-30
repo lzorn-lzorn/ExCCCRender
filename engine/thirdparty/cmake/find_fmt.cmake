23c024d6e041ed5500584f2db0cc01a882c2cb22
Lzorn <lzr18535482977@gmail.com>
2025-03-30 09:30:26 +0800

    ...

--- engine/thirdparty/cmake/find_fmt.cmake
+++ engine/thirdparty/cmake/find_fmt.cmake
@@ -1,3 +1,3 @@
-add_subdirectory(${ExCCCRender_THIRDPARTY_DIR}/fmt)
+add_subdirectory(${ExCCCRender_THIRDPARTY_DIR}/fmt) 
 target_link_libraries(${PROJECT_NAME} PUBLIC fmt::fmt)
 target_include_directories(${PROJECT_NAME} PUBLIC ${ExCCCRender_THIRDPARTY_DIR}/fmt/include)
