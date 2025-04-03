a36193593a5fe584a973221eac2374fb887db1ed
lizhuoran <liZhuoran1998@outlook.com>
2025-04-03 11:17:34 +0800

    测试assimp导入

--- engine/src/editor/main.cpp
+++ engine/src/editor/main.cpp
@@ -1,23 +1,29 @@
-#include <cstdint>
+
 #include <iostream>
-#include <array>
-#include "runtime/core/math/random/Random.h"
-#include "runtime/core/illumination/Ray.h"
+#include "assimp/Importer.hpp"
+#include "assimp/postprocess.h"
 #include "assimp/scene.h"
 #include <fmt/core.h>
-
-using namespace ExCCCRender::Core::Math;
-using namespace ExCCCRender::Core::Illumination;
 int main() {
-    for(size_t i = 0; i < 10; ++i){
-        std::cout << RandomGenerator::Instance().Generate<float>(0, 100, 10) << " ";
+    std::cout << "Hello Assimp" << std::endl;
+    std::cout.flush();
+    // 创建 Importer 实例
+    Assimp::Importer importer;
+
+    // 加载 .blend 文件，并应用后处理（如三角化、生成法线）
+    const aiScene* scene = importer.ReadFile(
+        "GirlWithAPearlEarring.blend",
+        aiProcess_Triangulate | aiProcess_GenNormals | aiProcess_FlipUVs
+    );
+
+    // 检查加载是否成功
+    if (!scene || !scene->mRootNode) {
+        std::cerr << "加载失败: " << importer.GetErrorString() << std::endl;
+        return -1;
     }
-    std::cout << std::endl;
 
-
-    Ray ray(Point3D(0, 0, 0), Direction(1, 0, 0), 1);
-
-    auto point = ray.At(0.5);
-
+    // 输出加载信息
+    std::cout << "成功加载网格数: " << scene->mNumMeshes << std::endl;
+    system("pause");
     return 0;
 }
